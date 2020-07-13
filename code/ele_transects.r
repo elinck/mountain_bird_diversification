bec5cbfa21a5279fe9f9f1ca213369ff00f16d58
Ignacio Quintero <ignacioquinterom@gmail.com>
2020-07-13 13:50:39 -0500

    code for making and extracting info from transects

--- code/ele_transects.r
+++ code/ele_transects.r
@@ -11,6 +11,8 @@
 library(raster)
 library(sp)
 library(rgeos)
+library(data.table)
+
 # load DEM
 r0 = raster('~/data/mountain_bird_diversification/data/mn30_grd/mn30_grd/w001001.adf')
 
@@ -27,15 +29,185 @@ e[[7]] = extent(-77.60973,-73.21778,1.448905 ,8.605978)
 # crop rasters
 r = lapply(e, function(x) crop(r0,x))
 
+# remove non-mountains
+mshp = readShapePoly('~/data/mountain_bird_diversification/data/MountSys/Global_GMBA.shp')
+
+for (i in 1:length(r)) {
+  exn = extract(r[[i]], mshp, cellnumbers = TRUE)
+  exn = exn[!sapply(exn, is.null)]
+  exn = sapply(exn, '[', , 1)
+  exn = na.omit(unlist(exn))
+  r[[i]][setdiff(1:ncell(r[[i]]), exn)] = NA
+  cat(i, '\n')
+}
+
+
 # make linear transects for all rectangles
 source('~/repos/mountain_bird_diversification/code/source.r')
 
-lss = lapply(r, make_transects, lowlands = 1000, buffer_width = 0.05)
+lss = lapply(r, make_transects, 
+  lowlands = 800, buffer_width = 0.05, min_transect_length = 500)
 
-save(lss, file = '~/data/mountain_bird_diversification/data/rect_transects.rda')
+save(r, lss, file = '~/data/mountain_bird_diversification/data/rect_transects.rda')
+
+# make array from list of lines
+lssv = list()
+
+for (j in 1:length(lss)) {
+  lsi  = lss[[j]]
+  lsiv = array(NA_real_, dim = c(length(lsi), 4))
+
+  lsiv[,1] = sapply(lsi, '[', 1, 1)
+  lsiv[,2] = sapply(lsi, '[', 1, 2)
+  lsiv[,3] = sapply(lsi, '[', 2, 1)
+  lsiv[,4] = sapply(lsi, '[', 2, 2)
+
+  lssv[[j]] = lsiv
+}
+
+# remove intersecting transects
+for (j in 1:length(lss)) {
+  lsi = lssv[[j]]
+  for (i1 in nrow(lsi):1) {
+    l1 = lsi[i1,]
+    if (anyNA(l1)) next
+    if (any(intersectV(l1, lsi[setdiff(1:nrow(lsi),i1),]))) {
+      lsi[i1,] = 0.0
+    }
+  }
+  lsi = lsi[!(lsi[,1] == lsi[,3] & lsi[,2] == lsi[,4]),]
+  # remove NAs
+  lssv[[j]] = lsi
+  cat(j, '\n')
+}
+
+
+# plot results
+par(mfrow = c(2,4))
+for (j in 1:length(r)) {
+  plot(r[[j]])
+  lsi = lssv[[j]]
+  segments(lsi[,1], lsi[,2], lsi[,3], lsi[,4])
+}
+
+
+# extract temperature
+source('~/repos/mountain_bird_diversification/code/source.r')
+load('~/data/mountain_bird_diversification/data/rect_transects.rda')
+
+
+# make stack of temperatures
+fmin = list.files('~/data/mountain_bird_diversification/data/tmin/',
+ full.names = TRUE)
+fmax = list.files('~/data/mountain_bird_diversification/data/tmax/', 
+  full.names = TRUE)
+smin = stack(fmin)
+smax = stack(fmax)
+
+tr = stack(smin, smax)
+
+# load extents
+e = list()
+e[[1]] = extent(-83.94194,-82.54267,35.02879,35.71528)
+e[[2]] = extent(-81.19186,-78.70011,37.08391,39.41059)
+e[[3]] = extent(-74.8787,-69.28792,42.51517,45.42436)
+e[[4]] = extent(-67.74567,-62.75549,-22.72413,-15.65367)
+e[[5]] = extent(-76.00255,-68.58037,-18.61333,-14.04033)
+e[[6]] = extent(-81.99395,-75.93518,-4.268851,2.284316)
+e[[7]] = extent(-77.60973,-73.21778,1.448905 ,8.605978)
+
+# crop rasters
+tr = lapply(e, function(x) crop(tr,x))
+
+# add elevation
+r = mapply(stack, r, tr)
+
+## extract Temperatures for each line
+
+# make list from array of lines
+lss = list()
+
+for (j in 1:length(lssv)) {
+  lsi  = lssv[[j]]
+
+  li = list()
+  for (i in 1:nrow(lsi)) {
+    li[[i]] = matrix(lsi[i,], ncol = 2, nrow = 2, byrow = TRUE)
+  }
+
+  lss[[j]] = li
+}
+
+# make spatial lines and extract values
+ex = list()
+for (j in 1:length(lss)) {
+  lsi = lss[[j]]
+  lsi = lapply(lsi, Line)
+  for (i in 1:length(lsi)) {
+    lsi[[i]] = Lines(lsi[[i]], ID = i)
+  }
+  spl = SpatialLines(lsi)
+  proj4string(spl) = proj4string(r[[j]])
+  ex[[j]] = extract(r[[j]],spl)
+  cat(j, '\n')
+}
+
+# Estimate temperature and elevation overlap/distance
+to = list()
+ed = list()
+for (j in 1:length(ex)) {
+  exi = ex[[j]]
+  toi = list()
+  edi = list()
+  for (i in 1:length(exi)) {
+    dt = exi[[i]]
+    toi[[i]] = toverlapM(dt[,2:25])
+    edi[[i]] = dist(dt[,1])
+  }
+  to[[j]] = toi
+  ed[[j]] = edi
+  cat(j, '\n')
+}
+
+save(to, ed,
+  file = '~/data/mountain_bird_diversification/results/rect_transects_toverlap.rda')
+
+# make plots
+load('~/data/mountain_bird_diversification/results/rect_transects_toverlap.rda')
+
+
+jpeg(file = '~/data/mountain_bird_diversification/plots/rect_transects_toverlap.jpeg',
+  width = 1000, height = 500)
+  par(mfrow = c(2,4), bty = 'n', las = 3)
+
+  for (j in 1:length(to)) {
+
+    toi = to[[j]]
+    edi = ed[[j]]
+
+    plot(1, type = 'n', xlim = c(0,4000), ylim = c(0,12),
+      xlab = 'Distance in elevation (m)', ylab = 'Temperature overlap')
+
+    for (i in 1:length(toi)) {
+      d1 = to[[j]][[i]]
+      d2 = ed[[j]][[i]]
+
+      x = as.vector(d2)
+      y = d1[lower.tri(d1)]
+
+      ord = order(x)
+      x = x[ord]
+      y = y[ord]
+
+      1/length(toi)
+
+      lines(x, y, col = rgb(0,0,0,5/length(toi)), lwd = 2)
+    }
+  }
+dev.off()
 
 
 
-plot(ri)
 
-lapply(lss, lines)
+
+
--- code/source.r
+++ code/source.r
@@ -7,8 +7,12 @@
 # 12 07 2020
 #
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+library(raster)
+library(sp)
+library(rgeos)
+library(data.table)
 
-make_transects = function(ri, lowlands = 1000, buffer_width = 0.05) {
+make_transects = function(ri, lowlands = 1000, buffer_width = 0.05, min_transect_length = 500) {
 
   lns = list()
   i   = 0
@@ -24,19 +28,25 @@ make_transects = function(ri, lowlands = 1000, buffer_width = 0.05) {
     # set highest point
     mxv = maxValue(ri)
 
-    print(mxv)
-
-    if (mxv <= lowlands) break
+    if (mxv <= (lowlands + min_transect_length)) break
 
     l[1,] = xyFromCell(ri, cell = which(ri[] == mxv))[1,]
     dfp = distanceFromPoints(ri, l[1,])
     dfp[ri >= lowlands] = NA
+    dfp[is.na(ri)] = NA
 
     # set closest lowland point
-    l[2,] = xyFromCell(ri, cell = which.min(dfp))[1,]
+    wmncell = which.min(dfp)
+    if (is.na(wmncell)) break
+    l[2,] = xyFromCell(ri, cell = wmncell)[1,]
+
+    mnv = ri[wmncell]
 
     # save line
-    lns[[i]] = l
+    if ((mxv - mnv) > min_transect_length) {
+      lns[[i]] = l
+      cat('added transect (', mxv,'=>', mnv,') \n')
+    }
 
     # remove buffer around point
     rb = buffer(SpatialLines(list(Lines(Line(l), ID = 1))), 
@@ -44,12 +54,178 @@ make_transects = function(ri, lowlands = 1000, buffer_width = 0.05) {
     exn = extract(ri, rb, cellnumbers = TRUE)[[1]][,1]
     ri[exn] = NA
 
-    cat('successfully made', i, 'transect(s) \n')
+    cat('successfully eval', i, 'transect(s) \n')
   }
 
   return(lns)
 }
 
+
+
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Line intersection functions
+# if point r is on segment p->q
+on_segment = function(px, py, qx, qy, rx, ry) {
+  if (qx <= max(px, rx) && qx >= min(px, rx) && 
+      qy <= max(py, ry) && qy >= min(py, ry)) {
+    return(TRUE)  
+  } else {
+    return(FALSE)  
+  }
+}
+
+on_segmentV = function(px, py, qx, qy, rx, ry) {
+  qx <= ifelse(px > rx, px, rx) &
+  qx >= ifelse(px < rx, px, rx) &
+  qy <= ifelse(py > ry, py, ry) &
+  qy >= ifelse(py < ry, py, ry)
+}
+
+
+# orientation of p->q
+orientation = function(px, py, qx, qy, rx, ry) {
+  val = (qy - py) * (rx - qx) - (qx - px) * (ry - qy)
+    if (val == 0) return(0L) else {
+      if (val > 0) return (1L) else return(2L)
+    }
+}
+
+orientationV = function(px, py, qx, qy, rx, ry) {
+  val = (qy - py) * (rx - qx) - (qx - px) * (ry - qy)
+  ifelse(val == 0, 0, ifelse(val > 0, 1, 2))
+}
+
+
+
+intersectV = function(l1, l2) {
+  p1x = l1[1] 
+  p1y = l1[2]
+  q1x = l1[3]
+  q1y = l1[4]
+
+  p2x = l2[,1]
+  p2y = l2[,2]
+  q2x = l2[,3]
+  q2y = l2[,4]
+
+  o1 = orientationV(p1x, p1y, q1x, q1y, p2x, p2y)
+  o2 = orientationV(p1x, p1y, q1x, q1y, q2x, q2y)
+  o3 = orientationV(p2x, p2y, q2x, q2y, p1x, p1y)
+  o4 = orientationV(p2x, p2y, q2x, q2y, q1x, q1y)
+
+  b = logical(nrow(l2))
+  b = b + (o1 != o2 & o3 != o4)
+
+  #Special Cases 
+  # p1, q1 and p2 are colinear and p2 lies on segment p1q1 
+  b = b + (o1 == 0 & on_segmentV(p1x, p1y, p2x, p2y, q1x, q1y))
+
+  # p1, q1 and q2 are colinear and q2 lies on segment p1q1 
+  b = b + (o2 == 0 & on_segmentV(p1x, p1y, q2x, q2y, q1x, q1y))
+
+  # p2, q2 and p1 are colinear and p1 lies on segment p2q2 
+  b = b + (o3 == 0 & on_segmentV(p2x, p2y, p1x, p1y, q2x, q2y))
+
+  # p2, q2 and q1 are colinear and q1 lies on segment p2q2 
+  b = b + (o4 == 0 & on_segmentV(p2x, p2y, q1x, q1y, q2x, q2y))
+
+  return(b > 0)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# returns TRUE if line l1 intersects with line l2, FALSE otherwise
+intersect = function(l1, l2) {
+  p1x = l1[1,1] 
+  p1y = l1[1,2]
+  q1x = l1[2,1]
+  q1y = l1[2,2]
+  p2x = l2[1,1]
+  p2y = l2[1,2]
+  q2x = l2[2,1]
+  q2y = l2[2,2]
+
+  o1 = orientation(p1x, p1y, q1x, q1y, p2x, p2y)
+  o2 = orientation(p1x, p1y, q1x, q1y, q2x, q2y)
+  o3 = orientation(p2x, p2y, q2x, q2y, p1x, p1y)
+  o4 = orientation(p2x, p2y, q2x, q2y, q1x, q1y)
+
+  if (o1 != o2 && o3 != o4) return(TRUE) 
+
+  #Special Cases 
+  # p1, q1 and p2 are colinear and p2 lies on segment p1q1 
+  if (o1 == 0 && on_segment(p1x, p1y, p2x, p2y, q1x, q1y)) return(TRUE) 
+
+  # p1, q1 and q2 are colinear and q2 lies on segment p1q1 
+  if (o2 == 0 && on_segment(p1x, p1y, q2x, q2y, q1x, q1y)) return(TRUE) 
+
+  # p2, q2 and p1 are colinear and p1 lies on segment p2q2 
+  if (o3 == 0 && on_segment(p2x, p2y, p1x, p1y, q2x, q2y)) return(TRUE) 
+
+  # p2, q2 and q1 are colinear and q1 lies on segment p2q2 
+  if (o4 == 0 && on_segment(p2x, p2y, q1x, q1y, q2x, q2y)) return(TRUE) 
+
+  return (FALSE)
+}
+
+
+#vectorized function to calculate overlap
+# t1mn = vector of minimum monthly temperature for site 1
+# t1mx = vector of maximum monthly temperature for site 1
+# t2mn = vector of minimum monthly temperature for site 2
+# t2mx = vector of maximum monthly temperature for site 2
+toverlap = function(t1mn, t1mx, t2mn, t2mx) {
+
+  r1 = t1mx - t1mn
+  r2 = t2mx - t2mn
+
+  max1 = pmax(t1mx, t1mn)
+  min1 = pmin(t1mx, t1mn)
+  max2 = pmax(t2mx, t2mn)
+  min2 = pmin(t2mx, t2mn)
+
+  ov1 = (pmin(max1,max2) - pmax(min1,min2))/r1
+  ov2 = (pmin(max1,max2) - pmax(min1,min2))/r2
+  wz = max1 < min2 | max2 < min1
+  ov1[wz] = ov2[wz] = 0.0
+
+  return(sum((ov1+ov2)/2))
+}
+
+
+# estimate toverlap matrix for a data frame with 24 columns
+# arranged as minimum monthly temperature and then maximum monthly temperature
+toverlapM = function(dt){
+
+  ovM = array(NA_real_, dim = c(nrow(dt), nrow(dt)))
+
+  for (j in 1:nrow(dt)) {
+    for (i in 1:j) {
+      if (i == j) {
+        ovM[j,i] = 12.0
+      } else {
+        ovM[j,i] = toverlap(dt[i,1:12], dt[i,13:24], dt[j,1:12], dt[j,13:24])
+      }
+    }
+  }
+
+  return(ovM)
+}
 
 
 
